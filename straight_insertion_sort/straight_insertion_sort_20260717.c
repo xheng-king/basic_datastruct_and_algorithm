2ea3308294b6b9c6400fa874e54d9d0cb45fc8ec
Youngyking <yy3011698382@gmail.com>
2026-07-17 18:16:04 +0800

    straight insertion sort

--- straight_insertion_sort/straight_insertion_sort_20260717.c
+++ straight_insertion_sort/straight_insertion_sort_20260717.c
@@ -1,5 +1,5 @@
 /*
- * File: straight_insertion_sort_20260716.c
+ * File: straight_insertion_sort_20260717.c
  * Purpose: Sort an integer array in non-decreasing order using straight insertion sort
  *          with a sentinel at index 0 (the actual data starts from index 1).
  * Input:  The user is prompted to enter the length of the array, followed by that
@@ -13,11 +13,10 @@
 void insert_sort(int arr[], int length){
     for(int i = 2; i <= length; i++){
         if(arr[i] < arr[i-1]){
-            //need insertion
             arr[0] = arr[i];
             arr[i] = arr[i-1];
             int j;
-            for(j = i-2; arr[j] > arr[0]; j--){
+            for(j = i-2; arr[0] < arr[j]; j--){
                 arr[j+1] = arr[j];
             }
             arr[j+1] = arr[0];
@@ -26,20 +25,21 @@ void insert_sort(int arr[], int length){
 }
 
 int main(){
-    int length = 0;
-    
-    printf("length of array:");
+    int length;
+
+    printf("input length of array:");
     scanf("%d", &length);
 
-    int *array = (int *)malloc(sizeof(int) * (length + 1));
-    printf("array:");
+    int *array = (int *)malloc(sizeof(int)*(length+1));
+
+    printf("input array:");
     for(int i = 1; i <= length; i++){
         scanf(" %d", &array[i]);
     }
-    
+
     insert_sort(array, length);
 
-    printf("sorted:");
+    printf("sorted array:");
     for(int i = 1; i <= length; i++){
         printf("%d ", array[i]);
     }
